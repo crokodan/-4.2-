540211818e89bceadafeb4d59eba0a60bdd48bcf
Piholot2K (daniilhonda) <69081378+crokodan@users.noreply.github.com>
2024-02-16 19:11:57 +0400

    Add files via upload

---
+++ Задание 4.2 Средний Вар 2/Задание 4.2 Средний Вар 2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задание_4._2_Средний_Вар_2
+{
+    internal class Program
+    {
+        const int N = 4;
+        static void Main()
+        {
+            int[,] a = new int[N, N];
+            for (int i = 0; i < N; ++i)
+                for (int j = 0; j < N; ++j)
+                    if ((i + j) % 2 == 0)
+                        a[i, j] = 0;
+                    else
+                        a[i, j] = 1;
+
+            for (int i = 0; i < N; ++i)
+            {
+                for (int j = 0; j < N; ++j)
+                    Console.Write(a[i, j] + " ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
